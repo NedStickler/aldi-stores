7404d7a7596ad5f5d3ce70e6f41aa0bdd3586041
Ned Stickler <nedstickler@gmail.com>
2024-08-10 22:16:34 +0100

    Initial working scrape

--- src/scraper.py
+++ src/scraper.py
@@ -1,24 +1,31 @@
 
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
+# TODO:
+# - Automated analysis of robots.txt
 
 if __name__ == "__main__":
     driver = webdriver.Chrome()
     driver.get("https://www.tesco.com/store-locator/directory")
 
-    directory_items = driver.find_elements(By.CLASS_NAME, "Directory-listLink")
-
+    stores_and_areas = driver.find_elements(By.CLASS_NAME, "Directory-listLink")
     stores = []
-    locations = []
-    for item in directory_items:
+    areas = []
+
+    # Sort stores and areas
+    for item in stores_and_areas:
         if int(item.get_attribute("data-count")[1:-1]) == 1:
             stores.append(item.get_attribute("href"))
         else:
-            locations.append(item.get_attribute("href"))
-    
-    for location in locations:
-        driver.get(location)
-        location_stores = driver.find_elements(By.CLASS_NAME, "Teaser-button")
-        for location_store in location_stores:
-            stores.append(location_store.get_attribute("href"))
+            areas.append(item.get_attribute("href"))
+
+    for area in areas:
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
+        res = requests.get(area, headers=headers)
+        soup = BeautifulSoup(res.text, "html.parser")
+        area_stores = soup.find_all(class_="Teaser-button")
+        for area_store in area_stores:
+            stores.append("https://www.tesco.com/store-locator" + area_store.get("href"))
